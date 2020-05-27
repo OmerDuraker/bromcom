b12fe999124ba2138e4796eee39e35e613ed0097
iugo-omer-duraker <55092790+iugo-omer-duraker@users.noreply.github.com>
2020-05-27 20:50:47 +0300

    review slider added

--- assets/plugins/scripts.js
+++ assets/plugins/scripts.js
@@ -61,16 +61,35 @@
     },
   });
 
-  /* Text Slider - Swiper */
-  var textSlider = new Swiper(".text-slider", {
+  var imageSlider = new Swiper(".image-slider2", {
     autoplay: {
-      delay: 6000,
+      delay: 2500,
       disableOnInteraction: false,
     },
     loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    spaceBetween: 30,
+    slidesPerView: 3,
+    breakpoints: {
+      // when window is <= 580px
+      580: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      // when window is <= 768px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      // when window is <= 992px
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      // when window is <= 1200px
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
     },
   });
 })(jQuery);
